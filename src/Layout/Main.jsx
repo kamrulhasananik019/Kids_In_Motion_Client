45714f30675b68f010d0b2942a6d68185a1c61a6
unknown <kamrulhasananik019@gmail.com>
2023-06-12 15:38:14 +0600

    footer fix

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,15 +4,14 @@ import Footer from '../Pages/Shared/Footer/Footer';
 import { Outlet, useLocation } from 'react-router-dom';
 
 const Main = () => {
-  const location = useLocation();
 
-  const noHeaderFooter = location.pathname.includes('/');
+
 
   return (
     <div>
       <NavBar></NavBar>
       <Outlet></Outlet>
-      {noHeaderFooter || <Footer></Footer>}
+    <Footer></Footer>
     </div>
   );
 };
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -2,34 +2,31 @@ import React from 'react';
 
 const Footer = () => {
     return (
-
-        <footer className="footer p-10 mt-10 bg-[--nav-bg] text-base-content">
-            <div>
-                <img src="https://i.ibb.co/p2ZXTr4/kids-in-motion-summer-sports-camp-removebg-preview.png" width={90} alt="" />
-                <p className='font-bold text-2xl'>Kids In Motion</p>
-            </div>
-            <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Football</a>
-                <a className="link link-hover">Cricket</a>
-                <a className="link link-hover">Hokey</a>
-                <a className="link link-hover"></a>
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </div>
-        </footer>
-
+            <footer className="footer mt-10 py-10 px-28 bg-[--nav-bg] text-base-content">
+                <div>
+                    <img src="https://i.ibb.co/p2ZXTr4/kids-in-motion-summer-sports-camp-removebg-preview.png" width={90} alt="" />
+                    <p className='font-bold text-2xl'>Kids In Motion</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Football</a>
+                    <a className="link link-hover">Cricket</a>
+                    <a className="link link-hover">Hokey</a>
+                    <a className="link link-hover"></a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+                <div>
+                    <span className="footer-title">Support</span>
+                    <a className="link link-hover">SUPPORT 24/7</a>
+                    <a className="link link-hover">Live Class</a>
+                    <a className="link link-hover">Trainers in ground</a>
+                </div>
+            </footer>
     );
 };
 
